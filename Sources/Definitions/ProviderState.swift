f90d61636af78b24a2286186c0b5b4d459788506
surpher <marko.justinek@gmail.com>
2020-04-02 20:36:28 +1100

    feat(pact) Add ProviderState model definition

---
+++ Sources/Definitions/ProviderState.swift
@@ -0,0 +1,16 @@
+//
+//  ProviderState.swift
+//  PactSwift
+//
+//  Created by Marko Justinek on 2/4/20.
+//  Copyright © 2020 PACT Foundation. All rights reserved.
+//
+
+import Foundation
+
+struct ProviderState {
+
+	let name: String
+	let params: [String: String]
+
+}
