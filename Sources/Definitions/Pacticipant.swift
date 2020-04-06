73014871b31f5c9a302b7a688726de721254d288
surpher <marko.justinek@gmail.com>
2020-04-06 12:31:53 +1000

    feat(Pact) Custom encoder for Pacticipant model

--- Sources/Definitions/Pacticipant.swift
+++ Sources/Definitions/Pacticipant.swift
@@ -13,11 +13,11 @@ public enum Pacticipant {
 	case consumer(String)
 	case provider(String)
 
-	var name: [String: String] {
+	var name: String {
 		switch self {
 		case .consumer(let name),
 				 .provider(let name):
-			return ["name": name]
+			return name
 		}
 	}
 
@@ -25,9 +25,13 @@ public enum Pacticipant {
 
 extension Pacticipant: Encodable {
 
+	enum CodingKeys: CodingKey {
+		case name
+	}
+
 	public func encode(to encoder: Encoder) throws {
-		var container = encoder.unkeyedContainer()
-		try container.encode(name)
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		try container.encode(name, forKey: .name)
 	}
 
 }
